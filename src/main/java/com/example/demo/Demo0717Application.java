d1fed4c84d15c834af7cc077e3551b919913d18c
huangzuojuan <13761006239@163.com>
2018-07-17 12:14:40 +0800

    init

---
+++ src/main/java/com/example/demo/Demo0717Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0717Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0717Application.class, args);
+    }
+}
